724e936234a208270455f3f740c250f5461bad52
algazahp <algazahp19@gmail.com>
2023-09-19 10:15:08 +0700

    SImple Calculator

---
+++ myScript.js
@@ -0,0 +1,30 @@
+function tambah() {
+  let angka1 = parseFloat(document.getElementById("angka1").value);
+  let angka2 = parseFloat(document.getElementById("angka2").value);
+  let hasil = angka1 + angka2;
+  document.getElementById("hasil").value = hasil;
+}
+
+// Mendefinisikan fungsi untuk operasi pengurangan
+function kurang() {
+  let angka1 = parseFloat(document.getElementById("angka1").value);
+  let angka2 = parseFloat(document.getElementById("angka2").value);
+  let hasil = angka1 - angka2;
+  document.getElementById("hasil").value = hasil;
+}
+
+// Mendefinisikan fungsi untuk operasi perkalian
+function kali() {
+  let angka1 = parseFloat(document.getElementById("angka1").value);
+  let angka2 = parseFloat(document.getElementById("angka2").value);
+  let hasil = angka1 * angka2;
+  document.getElementById("hasil").value = hasil;
+}
+
+// Mendefinisikan fungsi untuk operasi pembagian
+function bagi() {
+  let angka1 = parseFloat(document.getElementById("angka1").value);
+  let angka2 = parseFloat(document.getElementById("angka2").value);
+  let hasil = angka1 / angka2;
+  document.getElementById("hasil").value = hasil;
+}
